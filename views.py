baea35c4a8db921a91778d780a2c36549507428a
kervison <2388992177@qq.com>
2017-02-13 19:46:00 +0800

    'fixbug'

--- views.py
+++ views.py
@@ -7,11 +7,11 @@ def index(request):
 
 from django.shortcuts import render
 
-blocks_info = [
+block_info = [
     {'name':'运维专区','manager':'admin','desc':'运维人员看过来'},
     {'name':'django专区','manager':'admin','desc':'django学习讨论专区'},
     {'name':'部落建设','manager':'admin','desc':'有关部落建设事宜'}
 ]
 
 def index(request):
-    return render(request,'index.html','blocks':blocks_info)
+    return render(request,'index.html',{'blocks':block_info})

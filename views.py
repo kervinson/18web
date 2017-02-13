3f3da9ce5c63ad654528e8d61befc883075358a0
kervison <2388992177@qq.com>
2017-02-13 19:15:04 +0800

    'simplecode'

--- views.py
+++ views.py
@@ -7,5 +7,11 @@ def index(request):
 
 from django.shortcuts import render
 
+blocks_info = [
+    {'name':'运维专区','manager':'admin','desc':'运维人员看过来'},
+    {'name':'django专区','manager':'admin','desc':'django学习讨论专区'},
+    {'name':'部落建设','manager':'admin','desc':'有关部落建设事宜'}
+]
+
 def index(request):
-	return render(request,'index.html')
+    return render(request,'index.html')

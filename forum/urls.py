dce3b493e241f4825931625aee354f345e032e5d
kervison <2388992177@qq.com>
2017-02-13 09:26:29 +0800

    'bootstrpcopy'

--- forum/urls.py
+++ forum/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib import staticfiles
 import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^static/(?P<path>.*)$',django.contrib.staticfiles.views.serve),
+    url(r'^static/(?P<path>.*)$',staticfiles.views.serve),
     url(r'^$',views.index)
 ]

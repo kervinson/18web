a4ee50f15eb5ad744c0abc01e460c26fa8d2f408
kervison <2388992177@qq.com>
2017-02-14 08:56:00 +0800

    'changeblock'

--- block/model.py
+++
@@ -1,8 +0,0 @@
-#coding=utf-8
-from django.db import models
-
-class Block(models.Model):
-
-	name = models.CharField(u'板块名称',max_length='100')
-	manager = models.CharField(u'板块描述',max_length='100')
-	desc = models.CharField(u'管理员名称',max_length='100')
